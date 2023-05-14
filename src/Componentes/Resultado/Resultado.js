62562bd50b51bfe2ad41b9dce4f225442c2de4c4
Gabriel Medina <medina.rj@live.com>
2023-05-14 03:24:33 -0300

    v2.1 beta

--- src/Componentes/Resultado/Resultado.js
+++ src/Componentes/Resultado/Resultado.js
@@ -2,30 +2,30 @@ import './Resultado.scss'
 
 export const Resultado = (props) => {
     return (
-        <div className='col'>
+        <div className='container'>
             <div className='item row'>
                 <h3>Resultados da avaliação</h3>
-            <div className='col'>
-                <label>IMC</label>
-                <strong>{props.resultado['imc']}</strong>
+                <div className='col'>
+                    <label>IMC</label>
+                    <strong>{props.resultado['imc']}</strong>
+                </div>
+                <div className='col'>
+                    <label>%G 5 dobras</label>
+                    <strong>{props.resultado['pG5H']}</strong>
+                </div>
+                <div className='col'>
+                    <label>%G 7 dobras</label>
+                    <strong>{props.resultado['pG5H']}</strong>
+                </div>
+                <div className='col'>
+                    <label>Peso de Gordura</label>
+                    <strong>{props.resultado['pesoGorduraH']}</strong>
+                </div>
+                <div className='col'>
+                    <label>Peso Massa Magra</label>
+                    <strong>{props.resultado['lbmH']}</strong>
+                </div>
             </div>
-            <div className='col'>
-                <label>%G 5 dobras</label>
-                <strong>{props.resultado['pG5H']}</strong>
-            </div>
-            <div className='col'>
-                <label>%G 7 dobras</label>
-                <strong>{props.resultado['pG5H']}</strong>
-            </div>
-            <div className='col'>
-                <label>Peso de Gordura</label>
-                <strong>{props.resultado['pesoGorduraH']}</strong>
-            </div>
-            <div className='col'>
-                <label>Peso Massa Magra</label>
-                <strong>{props.resultado['lbmH']}</strong>
-            </div>
-        </div>
         </div>
     )
 }
